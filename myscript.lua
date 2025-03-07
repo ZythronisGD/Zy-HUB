8e0dce2a5275d20ee74563c8a4aeba0070ff0f53
ZythronisGD <issacrawcore572@gmail.com>
2025-03-07 09:12:41 -0300

    Update myscript.lua

--- myscript.lua
+++ myscript.lua
@@ -1,1 +1,9 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 
+local Window = OrionLib:MakeWindow({Name = "Zy-HUB", HidePremium = false, SaveConfig = true, ConfigFolder = "Zy-HUBConfig"})
+
+local Tab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://18155304028",
+	PremiumOnly = false
+})
